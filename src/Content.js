31d5fb8fe57422d55f1426177ca812cc53a8752c
Naoki Kano <nba102nk1029@icloud.com>
2022-02-08 00:09:35 +0900

    modify

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ItemList from './ItemList';
 
 const Content = ({ items, handleCheck, handleDelete }) => {
   return (
     <main>
       {items.length ? (
-          <ItemList
-            items={items}
-            handleCheck={handleCheck}
-            handleDelete={handleDelete}
-            />
+        <ItemList
+          items={items}
+          handleCheck={handleCheck}
+          handleDelete={handleDelete}
+        />
       ) : (
         <p style={{ marginTop: '2rem' }}>Your list is empty.</p>
       )}
